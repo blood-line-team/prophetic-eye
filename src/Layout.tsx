84a4ef6c29c89169ffb94603daaa801b4d3622dc
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2024-11-29 23:52:51 -0500

    feat: add layout to app

---
+++ src/Layout.tsx
@@ -0,0 +1,17 @@
+
+import { Outlet } from 'react-router-dom';
+
+import { SideBar } from './components/sidebar/SideBar';
+import { Group } from '@mantine/core';
+
+
+const Layout = () => {
+  return (
+    <Group h={'100%'} align='normal'>
+      <SideBar />
+      <Outlet />
+    </Group>
+  )
+}
+
+export default Layout
