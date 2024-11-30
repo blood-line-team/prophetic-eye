932ad4d679263d0e09d794ea0dda48c9ecadc629
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2024-11-30 00:00:27 -0500

    feat: add Client requirements component

---
+++ src/pages/Dashboard/ClientRequirement/ClientRequirement.tsx
@@ -0,0 +1,13 @@
+import { Stack, Title } from "@mantine/core";
+
+export default function ClientRequirement() {
+  return (
+    <Stack
+      px={20}
+      py={30}>
+      <Title>
+        Client Information
+      </Title>
+    </Stack>
+  )
+}
